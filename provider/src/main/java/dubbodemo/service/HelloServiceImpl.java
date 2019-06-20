300d6960d455f64d757d735589c753ba300704f6
sonofwinds <18939636972@163.com>
2019-06-20 20:09:31 +0800

    增加Provider注解

--- provider/src/main/java/dubbodemo/service/HelloServiceImpl.java
+++ provider/src/main/java/dubbodemo/service/HelloServiceImpl.java
@@ -3,13 +3,14 @@ package dubbodemo.service;
 import dubbodemo.contract.HelloService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service("helloService")
 public class HelloServiceImpl implements HelloService {
 private Logger logger = LoggerFactory.getLogger(HelloServiceImpl.class);
     @Override
     public String sayHello(String name) {
         logger.info("enter sayHello,name=" + name);
-        logger.info("99999999999");
         return name;
     }
 }
